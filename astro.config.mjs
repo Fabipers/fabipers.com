c724e1ce3b5501e12fcd389e913d22524b06a8f1
Fabian Perez <fabianperez@MacBook-Air-de-Fabian.local>
2026-07-21 20:50:10 -0500

    Perf/SEO: Configure CSS extraction, canonical links to production, and Google Fonts preconnects

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,5 +8,8 @@ import sitemap from '@astrojs/sitemap';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://fabipers.com',
-  integrations: [react(), sitemap()]
+  integrations: [react(), sitemap()],
+  build: {
+    inlineStylesheets: 'auto',
+  }
 });
